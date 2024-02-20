71a4912df03da6a3b19e68938ddcc359d3cfad3a
crodriguez988 <crodriguezg98@gmail.com>
2024-02-20 22:48:50 +0100

    Initial commit

---
+++ src/main/java/com/crodriguez/nextgame/NextGameApplication.java
@@ -0,0 +1,13 @@
+package com.crodriguez.nextgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crodriguez/nextgame/NextGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crodriguez.nextgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
